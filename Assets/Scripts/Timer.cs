6a812c819e9d1e9417cd8c52678b998940dbe632
Ivan Damjanović <damislav@net.hr>
2023-07-08 18:47:20 +0200

    first commit

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+
+
+    public bool loadNextQuestion;
+    public float fillFraction;
+    public bool isAnsweringQuestion = false;
+    float timerValue;
+
+    private void Update()
+    {
+        UpdateTimer();
+    }
+    public void CancelTimer()
+    {
+        timerValue = 0;
+    }
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+
+
+        if (isAnsweringQuestion)
+        {
+            // answering question
+            if (timerValue > 0)
+            {
+                //fill circle
+                fillFraction = timerValue / timeToCompleteQuestion; //5/10=0.5f
+            }
+            else
+            {
+                isAnsweringQuestion = false;
+                timerValue = timeToShowCorrectAnswer;
+
+            }
+
+        }
+        else
+        //not answering question
+        {
+            if (timerValue > 0)
+            {
+                //fill circle
+                fillFraction = timerValue / timeToShowCorrectAnswer; //5/10=0.5f
+            }
+            else
+            {
+                isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+                loadNextQuestion = true;
+            }
+        }
+
+        Debug.Log(isAnsweringQuestion + ": " + timerValue + ": " + fillFraction);
+    }
+}
